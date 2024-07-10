28ac26e0a652f82cef023d5e4d84a6f7e84368d0
Maribel <maribelsoledadalarcon@gmail.com>
2024-07-10 11:55:15 +0200

    add style modification

---
+++ game.js
@@ -0,0 +1,8 @@
+const stone = document.getElementById("stone");
+const paper = document.getElementById("paper");
+const scissors = document.getElementById("scissors");
+const userScore = document.getElementById("user");
+const pcScore = document.getElementById("pc");
+
+userScore.innerHTML = 0;
+pcScore.innerHTML = 0;
